b7866ac479210ae866028ee0c6b8b81a8a4d4498
Robert Wilson <youmufan@gmail.com>
2018-03-06 18:55:46 -0500

    connected App.js with the Provider, added reducers to change color, added a color picker

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
+import { changeColor } from './index';
 
 class App extends Component {
   render() {
     return (
-      <div>Hello</div>
+      <div>
+        <div>
+          <button onClick= {()=> this.props.dispatch(changeColor('blue'))}>Set Color</button>
+        </div>
+        <div>
+          <input type="color" name="col_picker" id="col_picker"/>
+        </div>
+      </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {...state};
+}
+
+export default connect(mapStateToProps,null)(App);
--- src/index.js
+++ src/index.js
@@ -2,6 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialColor = 
+{
+    color: 'red'
+};
+
+
+const COLOR_CHANGE = 'COLOR_CHANGE';
+export const changeColor = (col) =>
+({
+    type: COLOR_CHANGE,
+    payload: { value: col }
+});
+
+const colorReducer = (state = initialColor, action) =>
+{
+  switch(action.type){
+
+    case COLOR_CHANGE:      
+      return {...state, color: action.payload.value};
+
+    default:
+      return state;
+  }
+};
+
+const store = createStore(colorReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+  
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
+
